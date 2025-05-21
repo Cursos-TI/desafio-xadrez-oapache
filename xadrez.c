a8ec6b07e96a37a6fb72c6ced8a5eb1d78d4cb6c
oapache <160090075+oapache@users.noreply.github.com>
2025-05-21 12:00:55 -0300

    Desafio: Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -1,31 +1,14 @@
-#include <stdio.h>
-
-
-
-
-
-int main() {
-    printf("---------------Iniciando movimentação da Torre---------------\n");
-    int i = 1;
-    while(i <= 5) {
-        printf("Torre: Direita\n");
-        i++;
+printf("Rainha: Esquerda\n");
     }
 
-    printf("---------------Iniciando movimentação do Bispo---------------\n");
-    int j = 1;
-    do {
-        printf("Bispo: Direita\n");
-        printf("Bispo: Cima\n");
-        j++;
-    } while (j <= 5);
+    printf("---------------Iniciando movimentação do cavalo---------------\n");
 
-    printf("---------------Iniciando movimentação da Rainha---------------\n");
-
-    for (int k = 1; k <= 8; k++) {
-        printf("Rainha: Esquerda\n");
+    for (int l = 1; l <= 1; l++) {
+        for(int m = 1; m <= 2; m++) {
+            printf("Cavalo: Cima\n");
+        }
+        printf("Cavalo: Direita\n");
     }
-
 
 
     return 0;
